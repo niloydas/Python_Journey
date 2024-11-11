33006165ad24b7a909a1191ab63b019fa7623ec1
Niloy Das <dniloy65@gmail.com>
2024-11-12 01:05:02 +0600

    sum of current and previous number

---
+++ Ex2.py
@@ -0,0 +1,9 @@
+CurrentNumber=0 
+PreviousNumber=0  
+sum =  0
+print("Current Number =",CurrentNumber,"Previous Number =",PreviousNumber,"sum =",sum)
+while(CurrentNumber<9):
+    CurrentNumber=CurrentNumber+1
+    PreviousNumber=CurrentNumber-1
+    sum=CurrentNumber+PreviousNumber
+    print("Current Number =",CurrentNumber,"Previous Number =",PreviousNumber,"sum =",sum) 
